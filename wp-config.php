04c389343dbcdf063cb7533fe6f4ac94a643bb6d
Mónica Rebordinos <monica.rebordinos@mimotic.com>
2023-05-11 16:44:24 +0200

    feat: update wp-config

--- wp-config.php
+++ wp-config.php
@@ -1,24 +1,25 @@
 <?php
+
+// BEGIN iThemes Security - No modifiques ni borres esta línea
+// iThemes Security Config Details: 2
+define( 'DISALLOW_FILE_EDIT', true ); // Desactivar editor de archivos - Seguridad > Ajustes > Ajustes WordPress > Editor de archivos
+// END iThemes Security - No modifiques ni borres esta línea
+
+define( 'ITSEC_ENCRYPTION_KEY', 'RFNtKFNFa08ldDp7KVVoT1lULTReLXtndE0+a0FbMkFgOTNWV3N7LUFwMGRrMXJRbU9sbGkpWH0rMU9udkt4Tg==' );
+
 /**
- * Mimotic  enviroment.
+ * Environment File.
  * DO NOT MODIFY !!!
- * @package mimotic.com
+ * @package mimotic.io
  */
-
 require_once __DIR__ . '/dotEnvReader.php';
-
-
-
 /**
  * Configuración básica de WordPress.
  *
- * Este archivo contiene las siguientes configuraciones: ajustes de MySQL,
- * prefijo de tablas,
+ * Este archivo contiene las siguientes configuraciones: ajustes de MySQL, prefijo de tablas,
  * claves secretas, idioma de WordPress y ABSPATH. Para obtener más información,
- * visita la página del Codex
- * {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} .
- * Los ajustes de MySQL te los proporcionará tu proveedor de alojamiento web.
+ * visita la página del Codex{@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} . Los ajustes de MySQL te los proporcionará tu proveedor de alojamiento web.
  *
  * This file is used by the wp-config.php creation script during the
  * installation. You don't have to use the web site, you can just copy this file
@@ -26,65 +27,39 @@ require_once __DIR__ . '/dotEnvReader.php';
  *
  * @package WordPress
  */
-
-
 // ** Ajustes de MySQL. Solicita estos datos a tu proveedor de alojamiento web. ** //
-/**
- * The name of the database for WordPress
- */
-define('DB_NAME', dotEnvReader('DB_NAME', 'mimoticdb'));
-
-/**
- * Database username
- */
-define('DB_USER', dotEnvReader('DB_USER', 'root'));
-
-/**
- * Database password
- */
-define('DB_PASSWORD', dotEnvReader('DB_PASSWORD', 'root'));
-
-/**
- * Hostname
- */
-define('DB_HOST', dotEnvReader('DB_HOST', 'www'));
-
-
-/**
- * Codificación de caracteres para la base de datos.
- */
+/** El nombre de tu base de datos de WordPress */
+/** The name of the database for WordPress */
+define('DB_NAME', dotEnvReader('DB_NAME','mimotic') ); //
+/** MySQL database username */
+define('DB_USER', dotEnvReader('DB_USER','root'));
+/** MySQL database password */
+define('DB_PASSWORD', dotEnvReader('DB_PASSWORD','root'));
+/** MySQL hostname */
+define('DB_HOST', dotEnvReader('DB_HOST','www'));
+/** Codificación de caracteres para la base de datos. */
 define('DB_CHARSET', 'utf8');
-
-
-/**
- * l¡Lang default.
- */
-define('WPLANG', 'es_ES');
-
-
-
-/**
+// lang default
+define ('WPLANG', 'es_ES');
+/**#@+
  * Claves únicas de autentificación.
  *
  * Define cada clave secreta con una frase aleatoria distinta.
- * Puedes generarlas usando el {@link https://api.wordpress.org/secret-key/1.1/salt/
- * servicio de claves secretas de WordPress}
- * Puedes cambiar las claves en cualquier momento para invalidar
- * todas las cookies existentes.
- * Esto forzará a todos los usuarios a volver a hacer login.
+ * Puedes generarlas usando el {@link https://api.wordpress.org/secret-key/1.1/salt/ servicio de claves secretas de WordPress}
+ * Puedes cambiar las claves en cualquier momento para invalidar todas las cookies existentes. Esto forzará a todos los usuarios a volver a hacer login.
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'a&XliJ$K;tze08Y~zGxGw#e- ?jyocb<c]xU{CRq.O1$?h/-1NE^$tE^Wg<eP]2+');
-define('SECURE_AUTH_KEY',  '{ycJ~rPw>Hzt/(/T_{n9ZPs/Nv-Jd_kVcQ*N(+;qHu;qO/L;JW$W@k_]%uRS4;Gi');
-define('LOGGED_IN_KEY',    'y)K{^0dkZCOMfhFT/rU:@yz>Cp^yOb_%GgSW>1c(OAyVo94M&;s|<YuAgja<;(~m');
-define('NONCE_KEY',        'm~tu.v_Z=ll$F>8R%<_^SB!< f1vGb/D)`{0/_cO|:cl -Ap{ysd9i@]$68jD!~Z');
-define('AUTH_SALT',        'EIs+/l1.bJGsu;:2YJq-#V? 5;VE$5orn?}l=<@:+a5Jn!LQN{L<-}f:4x^S6y&H');
-define('SECURE_AUTH_SALT', 'n7m+UN{<dtMDMr%WSz^l:@s?`)R97|L-`MMs@`:i2soOqBb]oFKB6^.AwBA8Ab(Q');
-define('LOGGED_IN_SALT',   'kwbSY>?U9Ks*Lc/%3sV{2U-?zhBmG@DHW` -lzYO-=9_:;cn`<.[.)~)U%w/`I}L');
-define('NONCE_SALT',       '4pE0$>2[/&@/nsk3>whE?TGk9g^_d[ >817f(Jm9[KtZW nQ#C0gNx~+ehvd5}.E');
-
-
+// last update 27-ABRIL-2023 by Mimotic
+define('AUTH_KEY',         '4C9=9D[Iy|<RT:dD)jK4R@pO85 c*xF0o)$!SBKU^R#ri^FP9gR|bh|+hphlxU|c');
+define('SECURE_AUTH_KEY',  '0>[1+v#S^.z`+sj#0Nqr~]&dPkA;tS^4t@%k0|DDw/HfIghD#{f!|2^&7>,C8Hi[');
+define('LOGGED_IN_KEY',    '9IL9?U6+VGy9)XPm-J^KHNz=D!1!St2JQVnr3OQ|a{iY7g|{G1rnq!ovyA@w<h[*');
+define('NONCE_KEY',        'o,2qF`$5myX8n,n{M|fzGP?Xi>I.,c@hUcU>8S.,1V_;w*Q+FE+W|Hjw.HrfjGl,');
+define('AUTH_SALT',        'Bef#^iR_1T+j/0owxqYlTBZ+u-U-!zhwBuNA*yz51oMOpfYBa8_8D1Vg:y448U5B');
+define('SECURE_AUTH_SALT', '*SP$7&p6Zg{,aMO1Z+*2CZ=V+1y-o(LB^Cw;5rL@kV 7Zs]u?kk]Sl(D,|@Q^A?Q');
+define('LOGGED_IN_SALT',   'LL> ErI}r^wH35=5kqS^das`WCJM.>Rp5g0Mo{Y++H$vIFr#krCim0x#k[)1fG-/');
+define('NONCE_SALT',       '/sR9bzs<XHImU6Sk:~rFOJl(^qZPqxFYC( wBp^ptJi?tzti$S8w<8[Hq&;(e`OT');
+/**#@-*/
 
 /**
  * Prefijo de la base de datos de WordPress.
@@ -92,69 +67,59 @@ define('NONCE_SALT',       '4pE0$>2[/&@/nsk3>whE?TGk9g^_d[ >817f(Jm9[KtZW nQ#C0g
  * Cambia el prefijo si deseas instalar multiples blogs en una sola base de datos.
  * Emplea solo números, letras y guión bajo.
  */
-$table_prefix = dotEnvReader('TABLE_PREFIX', 'mimo_');
-define('WP_HOME', dotEnvReader('SITE_URL', 'https://mimotic.com/'));
-define('WP_SITEURL', dotEnvReader('SITE_URL', 'https://mimotic.com/'));
-
-
+$table_prefix = dotEnvReader('TABLE_PREFIX','wp_');
+define('WP_HOME', dotEnvReader('SITE_URL','https://mimotic.com/') );
+define('WP_SITEURL', dotEnvReader('SITE_URL','https://mimotic.com/') );
 /**
- * Entornos de desarrollo
+ * Para desarrolladores: modo debug de WordPress.
+ *
+ * Cambia esto a true para activar la muestra de avisos durante el desarrollo.
+ * Se recomienda encarecidamente a los desarrolladores de temas y plugins que usen WP_DEBUG
+ * en sus entornos de desarrollo.
  */
-if (dotEnvReader('SITE_ENVIROMENT') === 'production') { // production server
-
+if(dotEnvReader('SITE_ENVIROMENT') === 'production'){ // production server
     //ssl
     define('FORCE_SSL_ADMIN', true);
     define('FORCE_SSL_LOGIN', true);
-
     // debug
     define('WP_DEBUG', false);
     define('SCRIPT_DEBUG', false);
-    @ini_set('display_errors', 0);
+    @ini_set('display_errors',0);
     define( 'WP_DEBUG_LOG', __DIR__ . '/logs/wp-production-debug.log' );
+    define('WP_DEBUG_LOG', false);
     define('WP_DEBUG_DISPLAY', false);
     define('SAVEQUERIES', false);
     if (!defined('WP_CACHE')) {
-        define( 'WP_CACHE', true );
-    }
-
-} else if (dotEnvReader('SITE_ENVIROMENT') === 'stagin') {
-
+            }
+    /* Compression */
+    define( 'COMPRESS_CSS',        true );
+    define( 'COMPRESS_SCRIPTS',    true );
+    define( 'CONCATENATE_SCRIPTS', true );
+    define( 'ENFORCE_GZIP',        true );
+}else if(dotEnvReader('SITE_ENVIROMENT') === 'stagin'){
     //ssl
     define('FORCE_SSL_ADMIN', true);
     define('FORCE_SSL_LOGIN', true);
-
     // debug
     define('WP_DEBUG', true);
     define('SCRIPT_DEBUG', true);
     define( 'WP_DEBUG_LOG', __DIR__ . '/logs/wp-staging-debug.log' );
+    define('WP_DEBUG_LOG', true);
     define('WP_DEBUG_DISPLAY', true);
     define('SAVEQUERIES', true);
 
-
-} else { // local or lax servers
-
+}else{ // local/stagin server
     // ssl
     define('FORCE_SSL_ADMIN', false);
     define('FORCE_SSL_LOGIN', false);
-
     // debug
-    define('WP_DEBUG', true);
-    define('SCRIPT_DEBUG', true);
+    define('WP_DEBUG', true );
+    define('SCRIPT_DEBUG', true );
     define( 'WP_DEBUG_LOG', __DIR__ . '/logs/wp-local-debug.log' );
+    define('WP_DEBUG_LOG', true);
     define('WP_DEBUG_DISPLAY', true);
     define('SAVEQUERIES', true);
 }
-
-
-/**
- * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
- * See https://codex.wordpress.org/Function_Reference/is_ssl#Notes
- */
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    $_SERVER['HTTPS'] = 'on';
-}
-
-
 /*
 |--------------------------------------------------------------------------
 | Add-on: SendGrid
@@ -165,33 +130,32 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
 | both you and your users.
 |
 */
-
-//define('SENDGRID_USERNAME', '' );
-//define('SENDGRID_PASSWORD', '');
-//define('SENDGRID_SEND_METHOD', 'api');
-
+//define('SENDGRID_API_KEY', dotEnvReader('SENDGRID_API_KEY','') );
+//define('SENDGRID_KEY', dotEnvReader('SENDGRID_USERNAME','') );
+//define('SENDGRID_USERNAME', dotEnvReader('SENDGRID_USERNAME','') );
+//define('SENDGRID_PASSWORD', dotEnvReader('SENDGRID_PASSWORD',''));
+//define('SENDGRID_SEND_METHOD', dotEnvReader('SENDGRID_SEND_METHOD','api'));
 //
-//define('SENDGRID_FROM_NAME', '');
-//define('SENDGRID_FROM_EMAIL', '');
-//define('SENDGRID_REPLY_TO', '');
-//define('SENDGRID_CATEGORIES', '');
-
+//define('SENDGRID_FROM_NAME', dotEnvReader('SENDGRID_FROM_NAME',''));
+//define('SENDGRID_FROM_EMAIL', dotEnvReader('SENDGRID_FROM_EMAIL',''));
+//define('SENDGRID_REPLY_TO', dotEnvReader('SENDGRID_REPLY_TO',''));
+//define('SENDGRID_CATEGORIES', dotEnvReader('SENDGRID_CATEGORIES',''));
 
 /*
 |--------------------------------------------------------------------------
-| AWS media deslocalizada S3 -  4 mimotic
+| AWS media deslocalizada S3
 |--------------------------------------------------------------------------
 |
 |
 */
-
+// AWS CREDENTIALS
+//define( 'AWS_ACCESS_KEY_ID', '');
+//define( 'AWS_SECRET_ACCESS_KEY', '');
 //define( 'AS3CF_SETTINGS', serialize( array(
 //    'provider' => 'aws',
 //    'access-key-id' => dotEnvReader('AWS_ACCESS_KEY_ID', '*********'),
 //    'secret-access-key' => dotEnvReader('AWS_SECRET_ACCESS_KEY', '*********'),
 //) ) );
-
-
 /*
 |--------------------------------------------------------------------------
 | PERFORMANCE
@@ -199,22 +163,17 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
 |
 |
 */
-
 define('DISABLE_WP_CRON', true);
 define('EMPTY_TRASH_DAYS', 0);
 define('WP_POST_REVISIONS', false);
 //define('WP_MEMORY_LIMIT', '256M');
-
 /*
-
 |--------------------------------------------------------------------------
 | MULTISITE
 |--------------------------------------------------------------------------
 |
 |
 */
-
-
 //define('WP_ALLOW_MULTISITE', dotEnvReader('WP_ALLOW_MULTISITE', false));
 //define('MULTISITE', dotEnvReader('MULTISITE', false));
 //define('SUBDOMAIN_INSTALL', dotEnvReader('SUBDOMAIN_INSTALL', false));
@@ -222,6 +181,15 @@ define('WP_POST_REVISIONS', false);
 //define('PATH_CURRENT_SITE', dotEnvReader('PATH_CURRENT_SITE', ''));
 //define('SITE_ID_CURRENT_SITE', dotEnvReader('SITE_ID_CURRENT_SITE', 0));
 //define('BLOG_ID_CURRENT_SITE', dotEnvReader('BLOG_ID_CURRENT_SITE', 0));
+/*
+|--------------------------------------------------------------------------
+| Iterms disable security
+|--------------------------------------------------------------------------
+|
+|
+*/
+// define('ITSEC_DISABLE_TWO_FACTOR', true); // access to config
+//define('ITSEC_DISABLE_MODULES', true); // remove two factor
 
 /*
 |--------------------------------------------------------------------------
@@ -234,20 +202,28 @@ define('DISALLOW_FILE_MODS', dotEnvReader('DISALLOW_FILE_MODS', true));
 if (!defined('DISALLOW_FILE_EDIT')) {
     define('DISALLOW_FILE_EDIT', true);
 }
-define('WP_AUTO_UPDATE_CORE', false);
+
+define( 'WP_AUTO_UPDATE_CORE', true );
 header('X-Frame-Options: SAMEORIGIN');
 header('X-Frame-Options: DENY');
 
+/**
+ * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
+ * See https://codex.wordpress.org/Function_Reference/is_ssl#Notes
+ */
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
 
 /* ¡Eso es todo, deja de editar! Feliz blogging */
-/**
- * WordPress absolute path to the Wordpress directory.
- */
+/** WordPress absolute path to the Wordpress directory. */
 if (!defined('ABSPATH')) {
     define('ABSPATH', dirname(__FILE__) . '/');
 }
 
-/**
- * Sets up WordPress vars and included files.
- */
+/** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
+
+
+
