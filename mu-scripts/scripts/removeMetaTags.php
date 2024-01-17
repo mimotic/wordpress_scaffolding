5c2670e394c4bdb12e97e29a3eb98fa926279320
mimotic <mcacho@mimotic.com>
2024-01-17 14:33:34 +0100

    feat: remove meta generator revoltion slider

--- mu-scripts/scripts/removeMetaTags.php
+++ mu-scripts/scripts/removeMetaTags.php
@@ -16,4 +16,7 @@ add_action( 'init', function () {
     remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
     remove_action( 'wp_print_styles', 'print_emoji_styles' );
     remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+    // Slider revolution
+    add_filter('revslider_meta_generator', '__return_empty_string');
 });
