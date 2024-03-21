5f5d196194c16391fad4df9620a41f27198a1669
mimotic <mcacho@mimotic.com>
2024-03-21 13:17:48 +0100

    fix: file extension duplicated

--- mu-scripts/init.php
+++ mu-scripts/init.php
@@ -7,4 +7,4 @@ require __DIR__ . '/scripts/security.php';
 require __DIR__ . '/scripts/removeMetaTags.php';
 require __DIR__ . '/scripts/removeJqueryMigrate.php';
 
-// require __DIR__ . '/scripts/allowSvg.php.php';
+// require __DIR__ . '/scripts/allowSvg.php';
