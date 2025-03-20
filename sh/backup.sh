3f0a1f7e3c751cc0622dea10c974e65922de0a96
mimotic <mcacho@mimotic.com>
2025-03-20 20:20:40 +0100

    feat: add git config to sh

--- sh/backup.sh
+++ sh/backup.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 route=$1
 cd ${route}
+
 rm -rf backups/${route}
 mkdir backups/${route}
 cd backups/${route}
+
 wp db dump --path=../../public --porcelain db.sql
+
+git config --global user.name bot
+git config --global user.email bot@mimotic.com
+
 git add db.sql -f
 git commit -m 'database server backup'
 git push
