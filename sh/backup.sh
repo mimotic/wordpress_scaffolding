93412559ec6bbc4a025c2c7f2a02df5ffa61a1ca
mimotic <mcacho@mimotic.com>
2026-06-23 12:08:31 +0200

    feat: compress sql db copies

--- sh/backup.sh
+++ sh/backup.sh
@@ -7,11 +7,12 @@ mkdir backups/${route}
 cd backups/${route}
 
 wp db dump --path=../../public --porcelain db.sql
+gzip -9 -f db.sql
 
 git config --global user.name bot
 git config --global user.email bot@mimotic.com
 git config --global core.editor vi
 
-git add db.sql -f
+git add db.sql.gz -f
 git commit -m 'database server backup'
 git push
